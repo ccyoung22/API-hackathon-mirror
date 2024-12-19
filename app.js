04bbd8699a810c790fa61ab58da403464e98d06d
Adam Hemmings <38017169+Makenshii@users.noreply.github.com>
2024-12-19 18:02:40 +0000

    updated functions to search by id

--- app.js
+++ app.js
@@ -1,28 +1,12 @@
-//import express to use as framework
 import express from "express";
-
-//import body parser module
 import bodyParser from "body-parser";
+import { getMovies, getMovieById, getMovieTitle } from "./config.js";
 
-//import database
-//import { data } from "./database.js" //with { type: "json"};
-
-//define port
 const PORT = 3000;
-
-//import functions
-import { getMovies, getMovieTitle } from "./config.js";
-
-//create our express server
 const app = express();
 
-//create a middle man to parse and make it available under req.body
 app.use(express.json());
-
 app.use(bodyParser.json());
-
-//export the app so it can be used in other files
-export default app;
 
 app.get("/", async function (req, res) {
   try {
@@ -35,17 +19,26 @@ app.get("/", async function (req, res) {
   }
 });
 
-//listen for a query parameter of a title
-//take the title
-//give that to our get movie title function
-//respond with movie list
+app.get("/:id", async function (req, res) {
+  try {
+    const id = parseInt(req.params.id);
+    const movie = await getMovieById(id);
+    res.json(movie);
+  } catch (error) {
+    res.status(404).json({
+      message: error.message
+    });
+  }
+});
 
 app.get("/:title", async function (req, res) {
   try {
     const { title } = req.query;
-    console.log(title);
-    const movieList = await getMovieTitle(title);
-    res.json(movieList);
+    if (!title) {
+      return res.status(400).json({ message: "Title query parameter is required" });
+    }
+    const movie = await getMovieTitle(title);
+    res.json(movie);
   } catch (error) {
     res.status(500).json({
       message: error.message,
@@ -53,10 +46,8 @@ app.get("/:title", async function (req, res) {
   }
 });
 
-//listen for request
-//access database
-//process request
-//generate response
 app.listen(PORT, () => {
   console.log("Server is running on http://localhost:3000");
 });
+
+export default app;
--- config.js
+++ config.js
@@ -1,23 +1,29 @@
-//import the data
 import { data } from "./database.js";
 
 export async function getMovies() {
   return data;
 }
 
-//create a export async function
-//that grabs the whole data list
-//and returns it
-
-//Request by Title
-//listen for request (titles)
-export async function getMovieTitle(search) {
-  const lowercase = search.toLowerCase();
-  return data.filter(({ title }) => {
-    return title.toLowerCase().includes(lowercase);
-  });
+export async function getMovieById(id) {
+  try {
+    const movie = data.find(movie => movie.id === id);
+    if (!movie) {
+      throw new Error("Movie not found");
+    }
+    return movie;
+  } catch (error) {
+    throw new Error(error.message);
+  }
 }
-//Access the database
-//grab the data by title
-//process data
-//return data
+
+export async function getMovieTitle(title) {
+  try {
+    const movie = data.find(movie => movie.title.toLowerCase() === title.toLowerCase());
+    if (!movie) {
+      throw new Error("Movie not found");
+    }
+    return movie;
+  } catch (error) {
+    throw new Error(error.message);
+  }
+}
