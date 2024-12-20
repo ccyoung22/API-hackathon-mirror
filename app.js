499010eaf39c622779f939e93e1a9fb2800d62a6
Adam Hemmings <38017169+Makenshii@users.noreply.github.com>
2024-12-20 09:16:16 +0000

    updated the app

--- app.js
+++ app.js
@@ -8,23 +8,33 @@ const app = express();
 app.use(express.json());
 app.use(bodyParser.json());
 
+//listen for a request
 app.get("/", async function (req, res) {
   try {
+    // access the database and grab all the movies
     const movieList = await getMovies();
+    // return the list of movies with all info 
     res.json(movieList);
   } catch (error) {
+    // throw error if user is unable to find the database
     res.status(404).json({
       message: error.message,
     });
   }
 });
 
+
+//listen for a request by id
 app.get("/:id", async function (req, res) {
   try {
+    // grab the id from the user querey paramaeter
     const id = parseInt(req.params.id);
+    // search and grab the movie based on the id provided
     const movie = await getMovieById(id);
+    // return the slected movie
     res.json(movie);
   } catch (error) {
+    // throw error if the movie was not found
     res.status(404).json({
       message: error.message
     });
